eedec88b54e0a7843a495c17bea6c2c432df1eba
gorlok <pablo.lillia@gmail.com>
2016-08-29 22:48:48 -0300

    first commit

---
+++ javaeeExample/src/main/java/com/sample/jee7/JAXRSConfiguration.java
@@ -0,0 +1,8 @@
+package com.sample.jee7;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath(value = "resources")
+public class JAXRSConfiguration extends Application {
+}
