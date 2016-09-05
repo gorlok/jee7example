dc38b194e622e5fd693efbc70393ebc7abf96d59
gorlok <pablo.lillia@gmail.com>
2016-09-04 21:32:29 -0300

    delete obsolete project folders

--- javaeeExample/src/main/java/com/sample/jee7/JAXRSConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.sample.jee7;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath(value = "rest")
-public class JAXRSConfiguration extends Application {
-}
